19b81d855075ba571fcdd00a10da6228c8fff015
Tim Gittos <tim@chromasplosion.com>
2021-12-25 21:58:58 -0600

    Actually properly configure prom. update platform requirements.

--- docker/matomo/config/config.ini.php
+++ docker/matomo/config/config.ini.php
@@ -1,6 +1,7 @@
 ; <?php exit; ?> DO NOT REMOVE THIS LINE
 ; file automatically generated or modified by Matomo; you can manually override the default values in global.ini.php by redefining them in this file.
 [database]
+; host = "db"
 host = "192.168.144.2"
 username = "mp_dev"
 password = "mp!QAZxsw2"
